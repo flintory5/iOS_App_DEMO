e1b96282c8a452ddedecf2050c356b83823e638d
Flint Ory <flintory@gmail.com>
2015-06-19 23:44:17 -0500

    initial

--- war/ViewController.swift
+++ war/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstCardImageView: UIImageView!
+    @IBOutlet weak var secondCardImageView: UIImageView!
+    @IBOutlet weak var playRoundButton: UIButton!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.playRoundButton.setTitle("Play", forState: UIControlState.Normal)
+        self.playRoundButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func playRoundTapped(sender: UIButton) {
+        var myDeck = deck()
+        self.firstCardImageView.image = UIImage(named: myDeck.pickCard())
+        self.secondCardImageView.image = UIImage(named: myDeck.pickCard())
+    }
+    
+    class deck {
+        
+        init(){
+        
+        }
+        var deck:[String] = ["ace_of_spades", "2_of_hearts", "8_of_hearts"]
+        
+        func pickCard() -> String {
+            var randomCardNumber:Int = Int(arc4random_uniform(3))
+            return deck[randomCardNumber]
+        }
+    }
 
 }
 
